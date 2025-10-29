8fb55ce5f1c601c77bdc7d040a9ec44c1899b5ec
vishwab1 <vishwanath@navadhiti.com>
2025-10-29 21:28:02 +0530

    fix:casesheet signature

--- src/main/java/com/iemr/admin/service/employeemaster/EmployeeSignatureServiceImpl.java
+++ src/main/java/com/iemr/admin/service/employeemaster/EmployeeSignatureServiceImpl.java
@@ -65,7 +65,11 @@ public class EmployeeSignatureServiceImpl implements EmployeeSignatureService {
 
 	public Boolean existSignature(Long userID) {
 		// TODO Auto-generated method stub
-		return employeeSignatureRepo.countByUserIDAndSignatureNotNull(userID)>0;
+		return employeeSignatureRepo.countByUserIDAndSignatureNotNull(userID) > 0;
+	}
+
+	public Boolean isSignatureActive(Long userID) {
+		return employeeSignatureRepo.countByUserIDAndSignatureNotNullAndDeletedFalse(userID) > 0;
 	}
 
 	@Override
