ad6ff5eb3dd84de8a296752df7438580307419a8
Amoghavarsh <93114621+5Amogh@users.noreply.github.com>
2025-11-18 11:07:46 +0530

    Update regex handling for localhost URLs

--- src/main/java/com/iemr/admin/utils/http/HTTPRequestInterceptor.java
+++ src/main/java/com/iemr/admin/utils/http/HTTPRequestInterceptor.java
@@ -160,7 +160,8 @@ public class HTTPRequestInterceptor implements HandlerInterceptor {
 				.anyMatch(pattern -> {
 					String regex = pattern
 							.replace(".", "\\.")
-							.replace("*", ".*");
+							.replace("*", ".*")
+						    .replace("http://localhost:.*", "http://localhost:\\d+");
 					return origin.matches(regex);
 				});
 	}
