7a4411867175c8158fb824e0aff97a26af5afcf6
DurgaPrasad-54 <prasad8790237@gmail.com>
2026-02-12 14:04:07 +0530

    chore(swagger): update the default urls and properties

--- src/main/java/com/iemr/admin/config/SwaggerConfig.java
+++ src/main/java/com/iemr/admin/config/SwaggerConfig.java
@@ -11,13 +11,15 @@ import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
+
 public class SwaggerConfig {
+    private static final String DEFAULT_SERVER_URL = "http://localhost:9090";
 
     @Bean
     public OpenAPI customOpenAPI(Environment env) {
-        String devUrl = env.getProperty("api.dev.url", "http://localhost:9090");
-        String uatUrl = env.getProperty("api.uat.url", "http://localhost:9090");
-        String demoUrl = env.getProperty("api.demo.url", "http://localhost:9090");
+        String devUrl = env.getProperty("api.dev.url", DEFAULT_SERVER_URL);
+        String uatUrl = env.getProperty("api.uat.url", DEFAULT_SERVER_URL);
+        String demoUrl = env.getProperty("api.demo.url", DEFAULT_SERVER_URL);
         return new OpenAPI()
             .info(new Info().title("Admin API").version("version").description("Microservice for administration, configuration, user and role management, and service-level operations."))
             .addSecurityItem(new SecurityRequirement().addList("my security"))
