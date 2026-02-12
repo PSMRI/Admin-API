3be35dd05c117c50de1e03dd52ea1f78ae36f804
DurgaPrasad-54 <prasad8790237@gmail.com>
2026-02-12 12:25:44 +0530

    chore(swagger): add env-driven Dev/UAT/Demo servers

--- src/main/java/com/iemr/admin/config/SwaggerConfig.java
+++ src/main/java/com/iemr/admin/config/SwaggerConfig.java
@@ -2,6 +2,8 @@ package com.iemr.admin.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
@@ -11,14 +13,24 @@ import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
-	
-	@Bean
+    @Autowired
+    private Environment env;
+
+    @Bean
     public OpenAPI customOpenAPI() {
-        return new OpenAPI().info(new 
-             Info().title("Admin API").version("version").description("A microservice for the creation and management of beneficiaries."))
-                .addSecurityItem(new SecurityRequirement().addList("my security"))
-                .components(new Components().addSecuritySchemes("my security",
-                        new SecurityScheme().name("my security").type(SecurityScheme.Type.HTTP).scheme("bearer")));
+        String devUrl = env.getProperty("API_DEV_URL");
+        String uatUrl = env.getProperty("API_UAT_URL");
+        String demoUrl = env.getProperty("API_DEMO_URL");
+        return new OpenAPI()
+            .info(new Info().title("Admin API").version("version").description("A microservice for the creation and management of beneficiaries."))
+            .addSecurityItem(new SecurityRequirement().addList("my security"))
+            .components(new Components().addSecuritySchemes("my security",
+                new SecurityScheme().name("my security").type(SecurityScheme.Type.HTTP).scheme("bearer")))
+            .servers(java.util.Arrays.asList(
+                new io.swagger.v3.oas.models.servers.Server().url(devUrl).description("Dev"),
+                new io.swagger.v3.oas.models.servers.Server().url(uatUrl).description("UAT"),
+                new io.swagger.v3.oas.models.servers.Server().url(demoUrl).description("Demo")
+            ));
     }
 
 }
